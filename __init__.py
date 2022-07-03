f590b0d323c0118d426fe1a527c26c71d1cef439
interlark <interlark@gmail.com>
2022-07-03 22:06:02 +0500

    Export package methods for 3rd party apps

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 USTVGO Free IPTV.
 """
 
-from .ustvgo_iptv import main, VERSION
+from .ustvgo_iptv import main, args_parser, playlist_server, VERSION
 
 __version__ = VERSION
-__all__ = ['main', '__version__']
+__all__ = ['main', 'args_parser', 'playlist_server', '__version__']
