4f146e9821daa479381542ee8a35c23e2e6c3617
interlark <interlark@gmail.com>
2022-07-04 19:34:17 +0500

    Bump version

--- ustvgo_iptv.py
+++ ustvgo_iptv.py
@@ -35,7 +35,7 @@ Channel = TypedDict('Channel', {'id': int, 'stream_id': str, 'tvguide_id': str,
 # vlc http://127.0.0.1:6363/ustvgo.m3u8
 
 
-VERSION = '0.1.5'
+VERSION = '0.1.6'
 USER_AGENT = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
               '(KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36')
 USTVGO_HEADERS = {'Referer': 'https://ustvgo.tv', 'User-Agent': USER_AGENT}
